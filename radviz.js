89d07fa95b1c130cfd8fb117e0c3d57d6d6fd36f
ArjunCB28 <arjuncb28@gmail.com>
2019-10-12 18:53:08 -0300

    added transition and cosine function is now taking in degree instead of radiand

--- radviz.js
+++ radviz.js
@@ -1,6 +1,8 @@
 var WineData = [];
 // var columns = ["fixed acidity", "volatile acidity", "citric acid", "residual sugar", "chlorides", "free sulfur dioxide", "total sulfur dioxide", "density", "pH", "sulphates", "alcohol"];
 var columns = [];
+var selectedColumns = {};
+var selectedColumnsArr = [];
 var svgHeight = 800,
     svgWidth = 800;
 var anchorCX = 400;
@@ -18,11 +20,16 @@ var qualityColor = {
 
 
 // method to identify position to insert the anchors
-var attrPositionX = function(j, radius) {
-    return radius * Math.cos(j * 360 / columns.length) + anchorCX;
+// important: need to convert radians to degree to get correct values
+var attrPositionX = function(j, radius, m) {
+    // Math.sin((j * 360 / m) *Math.PI/180);
+
+    return radius * Math.cos((j * 360 / m) *Math.PI/180) + anchorCX;
+    // return radius * Math.cos(j * 360 / m) + anchorCX;
 }
-var attrPositionY = function(j, radius) {
-    return radius * Math.sin(j * 360 / columns.length) + anchorCY;
+var attrPositionY = function(j, radius, m) {
+    return radius * Math.sin((j * 360 / m) *Math.PI/180) + anchorCY;
+    // return radius * Math.sin(j * 360 / m) + anchorCY;
 }
 
 
@@ -47,7 +54,7 @@ function dragended(d) {
 
 var svg;
 var initialize = function() {
-    svg = d3.select("body").append("svg").attr("width", svgHeight).attr("height", svgHeight).attr("cx", 100).attr("cy", 100);
+    svg = d3.select("body").append("svg").attr("width", svgHeight).attr("height", svgHeight);
 
     // outer circle
     svg.selectAll("anchorCircle").append("anchorCircle");
@@ -58,123 +65,57 @@ var initialize = function() {
         .attr("cx", anchorCX)
         .attr("cy", anchorCY)
         .style("stroke", d => d.error ? "#F44336" : "#F44336")
+};
 
+var createAnchors = function(){
+    d3.selectAll("g").remove();
     var g = svg.selectAll("g")
-        .data(columns)
+        .data(selectedColumnsArr)
         .enter()
         .append("g")
-        // d3.select(".node").call(d3.drag().on("drag", mousemove));
-        // .attr("transform", (d,i)=>{
-        //     return "translate()";
-        // })
         .call(d3.drag().on("start", dragstarted).on("drag", dragged).on("end", dragended));
 
     g.append("circle")
-        // .data(columns)
-        // .enter()
-        // .append("circle")
         .attr("r", 10)
         .attr("cx", (d, i) => {
-            return attrPositionX(i, anchorCircleRadius);
+            return attrPositionX(i, anchorCircleRadius, selectedColumnsArr.length);
         })
         .attr("cy", (d, i) => {
-            return attrPositionY(i, anchorCircleRadius);
+            return attrPositionY(i, anchorCircleRadius, selectedColumnsArr.length);
         })
         .attr("fill", "#2196F3")
 
     g.append("text")
-        // .data(columns)
-        // .enter()
-        // .append("text")
         .text(function(d) {
             return d;
         })
         .attr("x", (d, i) => {
-            return attrPositionX(i, labelCircleRadius);
+            return attrPositionX(i, labelCircleRadius, selectedColumnsArr.length);
         })
         .attr("y", (d, i) => {
-            return attrPositionY(i, labelCircleRadius);
+            return attrPositionY(i, labelCircleRadius, selectedColumnsArr.length);
         })
         .attr("text-anchor", "middle")
 };
 
-
-
-
-// plotting points inside graph
-// function Attractor(name, x, y) {
-//     this.name = name
-//     this.x = x
-//     this.y = y
-// }
-
-
-// function DataPoint(attractions, quality) {
-//     this.attractions = attractions
-//     this.quality = quality
-//     this.totalAttractorForce = function() {
-//         return this.attractions.map(function(a) {
-//             return a.force
-//         }).reduce(function(a, b) {
-//             return a + b
-//         })
-//     }
-//     this.coordinateX = function() {
-//         return this.attractions.map(function(a) {
-//             return a.force * a.attractor.x
-//         }).reduce(function(a, b) {
-//             return a + b
-//         }) / this.totalAttractorForce()
-//     }
-//     this.coordinateY = function() {
-//         return this.attractions.map(function(a) {
-//             return a.force * a.attractor.y
-//         }).reduce(function(a, b) {
-//             return a + b
-//         }) / this.totalAttractorForce()
-//     }
-//     this.coordinates = [this.coordinateX(), this.coordinateY()]
-// }
-
-
-// var attractorObj = {};
-// columns.forEach((column, index) => attractorObj[column] = new Attractor(column, attrPositionX(index, anchorCircleRadius), attrPositionY(index, anchorCircleRadius)));
-
-// var buildDataPointsObj = function(index) {
-//     // WineData
-//     return columns.reduce((prevValue, column) => {
-//         prevValue.push({
-//             attractor: attractorObj[column],
-//             force: +WineData[index][column]
-//         });
-//         return prevValue;
-//     }, []);
-// }
-
-
-// var datapoints = [];
-
+var datapoints;
+// method to construct the data points inside the circle.
 var constructDataPoints = function() {
-    // tbr
-    // WineData.forEach((item, index) => {
-    //     datapoints.push(new DataPoint(buildDataPointsObj(index), item.quality))
-    // });
-
     // data points
     svg.selectAll("points").append("points");
 
     // points inside the plot
-    var datapoints = svg.selectAll("points")
+    datapoints = svg.selectAll("points")
         .data(WineData)
         .enter()
         .append("circle")
         .attr("cx", function(d, i) {
-            return calcDataPointX(d, i);
-            // return d.coordinateX();
+            return 0;
+            // return calcDataPointX(d, i);
         })
         .attr("cy", function(d, i) {
-            return calcDataPointY(d, i);
-            // return d.coordinateY();
+            return 0;
+            // return calcDataPointY(d, i);
         })
         .attr("r", 5)
         .attr("fill", "red")
@@ -190,40 +131,120 @@ var constructDataPoints = function() {
         .attr("fill", d => {
             return qualityColor[d.quality];
         })
-        .attr("opacity", "0.4")
-    // .attr("style","z-index:10;");
+        .attr("opacity", "1");
 }
 
-// calculating data points position is taken from this article
+var transitionDataPoints = function(){
+    datapoints.transition()
+   .duration(500)
+   .attr("cx", function(d, i) {
+        return calcDataPointX(d, i);
+    })
+    .attr("cy", function(d, i) {
+        return calcDataPointY(d, i);
+    })
+};
+
+// formula for calculating data points position is taken from this article
 // https://www.researchgate.net/publication/224343330_Vectorized_Radviz_and_Its_Application_to_Multiple_Cluster_Datasets
 var calcDataPointX = function(data, index) {
     var num = 0,
         den = 0;
-    columns.forEach((column, j) => {
-        num += +data[column] * Math.cos(j + 1) * attrPositionX(j, anchorCircleRadius);
+    selectedColumnsArr.forEach((column, j) => {
+        // num += +data[column] * Math.cos(j + 1) * attrPositionX(j, anchorCircleRadius, columns.length);
+        num += +data[column] * Math.cos(j + 1) * selectedColumns[column].x;
         den += +data[column];
     });
-    console.log("posX " + (num / den));
     return num / den + anchorCircleRadius;
 };
 
 var calcDataPointY = function(data, index) {
     var num = 0,
         den = 0;
-    columns.forEach((column, j) => {
-        num += +data[column] * Math.sin(j + 1) * attrPositionY(j, anchorCircleRadius);
+    selectedColumnsArr.forEach((column, j) => {
+        // num += +data[column] * Math.sin(j + 1) * attrPositionY(j, anchorCircleRadius, columns.length);
+        num += +data[column] * Math.sin(j + 1) * selectedColumns[column].y;
         den += +data[column];
     });
     return num / den + anchorCircleRadius;
-    // return anchorCircleRadius * (num / den) + anchorCY;
+};
+
+var getColumnLength = function(){
+    return Object.keys(selectedColumns).length;
+};
+
+// 
+var calculateAnchorCoordinates = function(){
+    if(getColumnLength() === 0) {
+        return;
+    }
+    var index = 0;
+    for(var column in selectedColumns) {
+        selectedColumns[column].x = attrPositionX(index,anchorCircleRadius, getColumnLength());
+        selectedColumns[column].y = attrPositionY(index,anchorCircleRadius, getColumnLength());
+        index++;
+    }
+    console.log(JSON.stringify(selectedColumns));
+    createAnchors();
+    transitionDataPoints();
+};
+
+// callback function when column is added or removed
+var columnChaged = function(elem){
+    if(elem.checked){
+        selectedColumns[elem.value] = {};
+        selectedColumnsArr.push(elem.value);
+    } else {
+        delete selectedColumns[elem.value];
+        selectedColumnsArr = selectedColumnsArr.filter(column => column !== elem.value);
+    }
+    calculateAnchorCoordinates();
+};
+
+// adds column checkbox
+var constructColumnSelection = function(){
+    columns.forEach(column=>{
+        var input = document.createElement("input");
+        var label = document.createElement("label");
+        var br = document.createElement("br");
+        input.innerHTML = column;
+
+        var type = document.createAttribute("type");
+        var value = document.createAttribute("value");
+        var id = document.createAttribute("id");
+        var name = document.createAttribute("name");
+        var onclick = document.createAttribute("onclick");
+        var forAttr = document.createAttribute("for");
+        id.value = column;
+        name.value = column;
+        type.value = "checkbox";
+        value.value = column;
+        forAttr.value = column;
+        onclick.value = "columnChaged(this);";
+        label.innerHTML = column;
+
+        input.setAttributeNode(type);
+        input.setAttributeNode(value);
+        input.setAttributeNode(id);
+        input.setAttributeNode(name);
+        input.setAttributeNode(onclick);
+        label.setAttributeNode(forAttr);
+        document.getElementById("column-select").appendChild(input);
+        document.getElementById("column-select").appendChild(label);
+        document.getElementById("column-select").appendChild(br);
+    });
+
 };
 
 
 d3.csv("winequality-red.csv", data => {
     WineData = data;
     columns = Object.keys(WineData[0]);
-    console.log("data " + JSON.stringify(WineData[0]));
+    columns.pop();
+    console.log("columns " + JSON.stringify(columns));
     initialize();
+    createAnchors();
+    constructColumnSelection();
     constructDataPoints();
 });
 
